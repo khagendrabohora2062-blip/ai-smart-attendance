534242d6aa87f006e01ec3c4a89995656c76e4c5
Khagendra Bohora <khagendrabohora2062@gmail.com>
2026-08-30 23:07:19 +0545

    Prepare application for production deployment

--- app.py
+++ app.py
@@ -262,9 +262,8 @@ app = create_app()
 # =========================================================
 
 if __name__ == "__main__":
-
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
